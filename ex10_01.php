89a14acf44654542788977337aba4d3ce6bfd7eb
hitourebito <yuto0423777@gmail.com>
2019-10-02 16:21:37 +0900

    check $_POST["nick"]

--- ex10_01.php
+++ ex10_01.php
@@ -2,5 +2,16 @@
   $errmsg = array();
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") 
+  {
+    $nick = htmlspecialchars($_POST["nick"], ENT_QUOTES);
+
+    if (!strlen($nick)) 
+    {
+      $errmsg[] = "ニックネームが入力されていません";
+    }
+    else
+    {
+      setcookie("ex10_01[nick]", $nick, time() + 3 * 60);
+    }
   }
 ?>
